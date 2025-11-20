9911e1d214f762c3355e4a748dec2bc8ab20523b
Tasveer Bhoola <tasveerbhoola7@gmail.com>
2025-11-20 23:50:19 +0200

    Delete PROG6212 POE directory

--- PROG6212 POE/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PROG6212_POE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
